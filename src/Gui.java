b6bb73c13b877ece886443491e44dade5e49ef0a
nicola.sannino <nicola10106@gmail.com>
2023-02-07 12:10:23 +0100

    aggiunta ActionListener

--- src/Gui.java
+++ src/Gui.java
@@ -2,8 +2,9 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class Gui extends JFrame implements ActionListener{
+public class Gui extends JFrame {
     static JFrame f;
+    Calculator calculator = new Calculator();
 
     // create a textfield
     static JTextField l;
@@ -12,22 +13,19 @@ public class Gui extends JFrame implements ActionListener{
     String s0, s1, s2;
 
     // default constructor
-    Gui()
-    {
+    Gui() {
         s0 = s1 = s2 = "";
     }
 
     // main function
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         // create a frame
         f = new JFrame("calculator");
 
         try {
             // set look and feel
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
@@ -72,23 +70,117 @@ public class Gui extends JFrame implements ActionListener{
         JPanel p = new JPanel();
 
         // add action listeners
-        bm.addActionListener(c);
-        bd.addActionListener(c);
-        bs.addActionListener(c);
-        ba.addActionListener(c);
-        b9.addActionListener(c);
-        b8.addActionListener(c);
-        b7.addActionListener(c);
-        b6.addActionListener(c);
-        b5.addActionListener(c);
-        b4.addActionListener(c);
-        b3.addActionListener(c);
-        b2.addActionListener(c);
-        b1.addActionListener(c);
-        b0.addActionListener(c);
-        be.addActionListener(c);
-        beq.addActionListener(c);
-        beq1.addActionListener(c);
+        bm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
+
+        bd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        bs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        ba.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        b0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculator.setN1(valueOf(b0.getText()));
+            }
+        });
+
+        be.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        beq.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        beq1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
 
         // add elements to panel
         p.add(l);
@@ -119,81 +211,4 @@ public class Gui extends JFrame implements ActionListener{
         f.setSize(200, 220);
         f.show();
     }
-    public void actionPerformed(ActionEvent e)
-    {
-        String s = e.getActionCommand();
-
-        // if the value is a number
-        if ((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == '.') {
-            // if operand is present then add to second no
-            if (!s1.equals(""))
-                s2 = s2 + s;
-            else
-                s0 = s0 + s;
-
-            // set the value of text
-            l.setText(s0 + s1 + s2);
-        }
-        else if (s.charAt(0) == 'C') {
-            // clear the one letter
-            s0 = s1 = s2 = "";
-
-            // set the value of text
-            l.setText(s0 + s1 + s2);
-        }
-        else if (s.charAt(0) == '=') {
-
-            double te;
-
-            // store the value in 1st
-            if (s1.equals("+"))
-                te = (Double.parseDouble(s0) + Double.parseDouble(s2));
-            else if (s1.equals("-"))
-                te = (Double.parseDouble(s0) - Double.parseDouble(s2));
-            else if (s1.equals("/"))
-                te = (Double.parseDouble(s0) / Double.parseDouble(s2));
-            else
-                te = (Double.parseDouble(s0) * Double.parseDouble(s2));
-
-            // set the value of text
-            l.setText(s0 + s1 + s2 + "=" + te);
-
-            // convert it to string
-            s0 = Double.toString(te);
-
-            s1 = s2 = "";
-        }
-        else {
-            // if there was no operand
-            if (s1.equals("") || s2.equals(""))
-                s1 = s;
-                // else evaluate
-            else {
-                double te;
-
-                // store the value in 1st
-                if (s1.equals("+"))
-                    te = (Double.parseDouble(s0) + Double.parseDouble(s2));
-                else if (s1.equals("-"))
-                    te = (Double.parseDouble(s0) - Double.parseDouble(s2));
-                else if (s1.equals("/"))
-                    te = (Double.parseDouble(s0) / Double.parseDouble(s2));
-                else
-                    te = (Double.parseDouble(s0) * Double.parseDouble(s2));
-
-                // convert it to string
-                s0 = Double.toString(te);
-
-                // place the operator
-                s1 = s;
-
-                // make the operand blank
-                s2 = "";
-            }
-
-            // set the value of text
-            l.setText(s0 + s1 + s2);
-        }
-    }
 }
-
